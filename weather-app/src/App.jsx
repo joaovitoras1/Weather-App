b71f5a1d5eba50d6349a2f882dfd53463a2bf75e
João Vitor <jvaee742@gmail.com>
2026-09-14 09:33:05 -0300

    chore: Inicializa projeto e instala dependências

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="max-w-full min-h-screen border-box">
+      
+    </div>
+  )
+}
+
+export default App
---
+++ weather-app/vite.config.js
@@ -0,0 +1,20 @@
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+  ],
+  resolve: {
+  alias: {
+    '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'src'),
+  },
+},
+})
